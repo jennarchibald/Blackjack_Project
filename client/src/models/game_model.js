56be3f0758ff402806f008f27805d7b5f7d4bd25
Becky <rebeccajoynielsen@gmail.come>
2019-04-19 14:52:49 +0100

    rewritten model to match other models

--- client/src/models/game_model.js
+++ client/src/models/game_model.js
@@ -1,18 +1,25 @@
-const Game = function(deck){
-  this.deck = deck
-  this.playerHand = []
-  this.dealerHand = []
+const Deck = require('./deck_model.js')
+const Hand = require('./hand_model.js')
+
+const Game = function(){
+  this.deck = null
+  this.playerHand = null
+  this.dealerHand = null
 };
 
-// deals two cards to each person, by pushing a dealt card into an array
+// creates a new deck and deals two cards to each player
 Game.prototype.openingDeal = function(){
-  dealtCards = this.deck.takeCards(2);
-  this.playerHand.push(dealtCards);
-  dealerCards = this.deck.takeCards(2);
-  this.dealerHand.push(dealtCards);
-  this.determineWinner();
-};
+  this.deck = new Deck;
+  card1 = this.deck.getCard();
+  card2 = this.deck.getCard();
+  card3 = this.deck.getCard();
+  card4 = this.deck.getCard();
+  this.playerHand = new Hand(card1, card3);
+  this.dealerHand = new Hand(card2, card4);
 
+}
+
+//compares values of two hands and returns which is higher
 Game.prototype.determineWinner = function(){
   playerHand = this.playerHand.totalValue();
   dealerHand = this.dealerHand.totalValue();
