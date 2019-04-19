6e14bff92966cd70820e0c76aceee4cc5f084ec8
Jon Zarecki <jonzarecki@Jons-MacBook-Air.local>
2019-04-19 16:22:32 +0100

    model MVP complete

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,10 @@
+const Game = require('./models/game_model.js');
+
 document.addEventListener('DOMContentLoaded', ()=> {
   console.log('Javascript Loaded');
+
+  const game = new Game();
+
+  game.getDeck();
 
 });
--- client/src/models/deck_model.js
+++ client/src/models/deck_model.js
@@ -1,14 +1,6 @@
-const RequestHelper = require('../helpers/request_helper.js')
 
 const Deck = function() {
   this.cards = null;
-}
-
-Deck.prototype.getCardsFromDatabase = function() {
-  const request = new RequestHelper('http://localhost:3000/api/deck');
-  request.get().then((cards) => {
-    this.cards = cards
-  }).catch((err) => console.error(err));
 }
 
 Deck.prototype.getCard = function() {
